399b436d6cb2a13ebc41cd95d0d88869d0d51f4d
BrunaDewes <134643325+BrunaDewes@users.noreply.github.com>
2023-07-12 17:04:46 -0300

    Add files via upload

---
+++ tabelas_hash.py
@@ -0,0 +1,79 @@
+import csv
+
+DADOS = {
+    'VALOR': []
+}
+
+with open('Ajuste patrimonial - Saldos (R$).csv', newline='') as basedados:
+    leitor = csv.reader (basedados, delimiter=';', quotechar='|')
+    for linha in leitor:
+        DADOS['VALOR'].append(float(linha[1]))
+
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self,key,data):
+      hashvalue = self.hashfunction(key,len(self.slots))
+
+      if self.slots[hashvalue] == None:
+        self.slots[hashvalue] = key
+        self.data[hashvalue] = data
+      else:
+        if self.slots[hashvalue] == key:
+          self.data[hashvalue] = data  #replace
+        else:
+          nextslot = self.rehash(hashvalue,len(self.slots))
+          while self.slots[nextslot] != None and \
+                          self.slots[nextslot] != key:
+            nextslot = self.rehash(nextslot,len(self.slots))
+
+          if self.slots[nextslot] == None:
+            self.slots[nextslot]=key
+            self.data[nextslot]=data
+          else:
+            self.data[nextslot] = data #replace
+
+    def hashfunction(self,key,size):
+         return key % size
+
+    def rehash(self,oldhash,size):
+        return (oldhash+1)%size
+
+    def get(self,key):
+      startslot = self.hashfunction(key,len(self.slots))
+
+      data = None
+      stop = False
+      found = False
+      position = startslot
+      while self.slots[position] != None and  \
+                           not found and not stop:
+         if self.slots[position] == key:
+           found = True
+           data = self.data[position]
+         else:
+           position=self.rehash(position,len(self.slots))
+           if position == startslot:
+               stop = True
+      return data
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __setitem__(self,key,data):
+        self.put(key,data)
+
+H=HashTable()
+
+H[1] = DADOS['VALOR'][72]
+print(DADOS['VALOR'][72])
+
+H[1] = DADOS['VALOR'][130]
+print(DADOS['VALOR'][130])
+
+H[1] = DADOS['VALOR'][200]
+print(DADOS['VALOR'][200])
